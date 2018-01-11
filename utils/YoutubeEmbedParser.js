d13747a00ac2d04685320898d110a57731496a04
Estuardo Aguirre <EstuardoA25@gmail.com>
2018-01-11 12:30:14 -0600

    Utility to extract video id

---
+++ utils/YoutubeEmbedParser.js
@@ -0,0 +1,10 @@
+export const YoutubeEmbedParser = function(url) {
+  var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  var match = url.match(regExp);
+
+  if (match && match[2].length === 11) {
+    return match[2];
+  }
+}
+
+export default YoutubeEmbedParser;
