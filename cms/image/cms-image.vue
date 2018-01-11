45e88d97049929d0fc4f662d762a0275f5fe10ac
Estuardo Aguirre <EstuardoA25@gmail.com>
2018-01-11 12:33:59 -0600

    Component to render images from prismic

---
+++ cms/image/cms-image.vue
@@ -0,0 +1,19 @@
+<script>
+  export const cmsImage = {
+    render(createElement) {
+      return createElement('img', {
+        attrs: {
+          src: this.content.url
+        }
+      }, this.$slots.default)
+    },
+    props: {
+      content: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+
+  export default cmsImage;
+</script>
