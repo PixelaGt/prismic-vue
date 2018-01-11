068beea055355049e1a86a95eb6b1b97ebbb72ab
Estuardo Aguirre <EstuardoA25@gmail.com>
2018-01-11 12:35:19 -0600

    Component to render Keytext from prismic

---
+++ cms/keytext/cms-keytext.vue
@@ -0,0 +1,15 @@
+<script>
+  export const cmsKeytext = {
+    render(createElement) {
+      return createElement('span', this.content);
+    },
+    props: {
+      content: {
+        type: String,
+        required: true
+      }
+    }
+  }
+
+  export default cmsKeytext;
+</script>
