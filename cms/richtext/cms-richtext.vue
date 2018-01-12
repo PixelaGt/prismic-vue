1f417925c6910c3d8b0b831c826a83148ee05d19
Estuardo Aguirre <EstuardoA25@gmail.com>
2018-01-12 15:57:37 -0600

    Prismic-dom implemented for richtext

---
+++ cms/richtext/cms-richtext.vue
@@ -0,0 +1,22 @@
+<template><div v-html="richText"></div></template>
+<script>
+  import PrismicDOM from 'prismic-dom';
+
+  export const cmsRichtext = {
+    computed: {
+      richText() {
+       return PrismicDOM.RichText.asHtml(this.content);
+      }
+    },
+    render(createElement) {
+    },
+    props: {
+      content: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+
+  export default cmsRichtext;
+</script>
