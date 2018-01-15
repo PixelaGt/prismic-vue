ee14ed5e5136229cc3b74346439d9d02a83c5cc9
Estuardo Aguirre <EstuardoA25@gmail.com>
2018-01-15 15:09:20 -0600

    Route library package

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
-import * as Prismic from 'prismic-javascript';
+import { CMS } from './cms/api';
+import { CMSDate } from './cms/date/cms-date.vue';
+import { CMSEmbed } from './cms/embed/cms-embed.vue';
+import { CMSImage } from './cms/image/cms-image.vue';
+import { CMSKeytext } from './cms/keytext/cms-keytext.vue';
+import { CMSLink } from './cms/link/cms-link.vue';
+import { CMSRichtext } from './cms/richtext/cms-richtext.vue';
+import { CMSTitle } from './cms/title/cms-title.vue';
 
-export class CMS {
-  constructor(endpoint) {
-    if(!CMS.instance){
-      this.prismicEndpoint = endpoint;
-      CMS.instance = this;
-    }
-    return CMS.instance;
-  }
-
-  setEndpoint(newEndpoint) {
-    this.prismicEndpoint = newEndpoint;
-  }
-
-  getEndpoint() {
-    return this.prismicEndpoint;
-  }
-
-  getApi() {
-    return Prismic.getApi(this.prismicEndpoint);
-  }
-
-  fetch(query) {
-    return this.getApi()
-      .then(function(api) {
-        return api.query(query);
-      })
-  }
-
-  page(page, params) {
-    return this.getApi().then(function(api) {
-      return api.getSingle(page, params);
-    })
-  }
-
-  fetchByType(type) {
-    return this.getApi().then(function(api) {
-        return api.query( Prismic.Predicates.at('document.type', type), {});
-      })
-  }
+export {
+   CMS,
+   CMSDate,
+   CMSEmbed,
+   CMSImage,
+   CMSKeytext,
+   CMSLink,
+   CMSRichtext,
+   CMSTitle
 }
